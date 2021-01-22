fed0dc9028cfe8a9a9653aedb5ab5baeeb691fb7
k4zzi <pauldesbiendas@gmail.com>
2021-01-22 08:59:40 +0100

    loiclebg

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,170 +1,119 @@
 // Initialisation des variables
 let zeParent = document.querySelector("#animProjection"); // le conteneur de l'animation
 let isPlaying = false // booléen - indique si l'animation (et la musique) sont jouées
-let music = new Audio('assets/mp3/music.mp3') // objet HTMLAudioElement (cf. MDN)
+var sound = new Howl({
+    src: ['assets/mp3/potsu-letting-go.wav'],
+    autoplay: false,
+    loop: true,
+    volume: 0.5,
+  });
 
-let circle = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'transparent',
-    stroke: '#FF6800',
-    strokeWidth: 4.25,
-    scale: {
-        0: 1
-    },
-    duration: 1000,
-    delay: 1000,
-    scale: {
-        0: 4.24,
-        easing: 'cubic.in'
-    },
-    angle: {
-        0: 135
-    },
-})
-let petal1 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    radiusX: 2.5,
-    radiusY: 11.80,
-    left: 427,
-    top: 131,
-    fill: '#00FDFF',
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
+  window.onload = function() {
+    document.getElementById('display-4').color = 'blue';
+};
+// Les shapes...
+const burst = new mojs.Burst({
+  radius:    100,
+  scale: {0 : 4.5},
+  delay: 5000,
+  count:    10,
+  children: {
+    shape:      'polygon',
+    points:     5,
+    fill:       { 'red' : 'yellow' },
+    angle:      { 360: 0 },
+    duration:   3000,
+    delay:      5000
+  }
+});
 
-})
-let petal2 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 326,
-    top: 230,
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
+const burst1 = new mojs.Burst({
+  radius:   { 0: 100 },
+  count:    10,
+  children: {
+    shape:      'polygon',
+    points:     5,
+    fill:       { 'cyan' : 'red' },
+    angle:      { 360: 0 },
+    duration:   1000,
+    delay:      'stagger(0, 1000)'
+  }
+});
 
-    angle: {
-        0: 90
-    },
-    strokeDashoffset: {
-        '100%': '-100%'
-    },
-})
-let petal3 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: '#FF6800',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 528,
-    top: 230,
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
+const burst4 = new mojs.Burst({
+  top: '25%',
+  radius:   { 0: 100 },
+  count:    10,
+  children: {
+    shape:      'polygon',
+    points:     5,
+    fill:       { 'cyan' : 'red' },
+    angle:      { 360: 0 },
+    duration:   1000,
+    delay:      'stagger(0, 100)'
+  }
+});
 
-    angle: {
-        0: 90
-    },
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
-let petal4 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'blue',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 427,
-    top: 340,
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
-let petal5 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'yellow',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 350,
-    top: 309,
-    scale: {
-        0: 8
-    },
-    angle: {
-        0: 45
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
-let petal6 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'yellow',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 350,
-    top: 309,
-    scale: {
-        0: 8
-    },
-    angle: {
-        0: 270
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
+const burst5 = new mojs.Burst({
+  top: '75%',
+  radius:   { 0: 100 },
+  count:    10,
+  children: {
+    shape:      'polygon',
+    points:     5,
+    fill:       { 'cyan' : 'red' },
+    angle:      { 360: 0 },
+    duration:   1000,
+    delay:      'stagger(0, 100)'
+  }
+});
+
+const burst2= new mojs.Burst({
+  radius:   { 0: 100 },
+  count:    10,
+  left: '65%',
+  children: {
+    shape:      'polygon',
+    points:     5,
+    fill:       { 'cyan' : 'yellow' },
+    angle:      { 360: 0 },
+    duration:   1000,
+    delay:      'stagger(0, 100)'
+  }
+});
+
+const burst3 = new mojs.Burst({
+  left: '35%',
+  radius:   { 0: 100 },
+  count:    10,
+  children: {
+    shape:      'polygon',
+    points:     5,
+    fill:       { 'purple' : 'yellow' },
+    angle:      { 360: 0 },
+    duration:   1000,
+    delay:      'stagger(0, 100)'
+  }
+});
 
 
 // La timeline
 const timeline = new mojs.Timeline({
-
-    })
-    .add(circle)
-    .add(petal1)
-    .add(petal2)
-    .add(petal3)
-    .add(petal4)
-    .add(petal5)
-    .add(petal6)
+    repeat: 999
+})
+    .add(burst, burst1, burst2, burst3, burst4, burst5, burst6  ) // Chaque animation est ajoutée à la timeline
 
 document.querySelector("#animProjection").addEventListener("click", function () {
 
     if (isPlaying != true) {
-        music.play()
+        sound.play()
         timeline.play()
         isPlaying = true
     } else {
-        music.pause()
+        sound.pause()
         timeline.pause()
         isPlaying = false
     }
 
 });
+
