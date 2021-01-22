185c85efef8d5c99636d0e80fed708e7e89c52cc
thomas.bolot <tbolot@gmail.com>
2021-01-22 09:06:39 +0100

    Commit bolo

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,160 +1,80 @@
 // Initialisation des variables
 let zeParent = document.querySelector("#animProjection"); // le conteneur de l'animation
 let isPlaying = false // booléen - indique si l'animation (et la musique) sont jouées
-let music = new Audio('assets/mp3/music.mp3') // objet HTMLAudioElement (cf. MDN)
+let music = new Audio('assets/mp3/potsu-letting-go.wav') // objet HTMLAudioElement (cf. MDN)
 
-let circle = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'transparent',
-    stroke: '#FF6800',
-    strokeWidth: 4.25,
-    scale: {
-        0: 1
-    },
-    duration: 1000,
-    delay: 1000,
-    scale: {
-        0: 4.24,
-        easing: 'cubic.in'
-    },
-    angle: {
-        0: 135
-    },
-})
-let petal1 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    radiusX: 2.5,
-    radiusY: 11.80,
-    left: 427,
-    top: 131,
-    fill: '#00FDFF',
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
+// Les shapes...
+const CircleA = new mojs.Shape({
+    shape:        'circle',
+    right:          '40%',
+    fill:         'black',
+    radius:       50,
+    stroke:       { 'rgba(0,255,255, 1)' : 'magenta' },
+    strokeWidth:  { 15: 5 },
+    strokeDasharray: '100%',
+    strokeDashoffset: { '-100%' : '100%', easing:'circ.in' },
+    angle:        { 0: 360 },
+  
+    duration:    6000,
+    repeat:       999,
+  }).play();
 
-})
-let petal2 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 326,
-    top: 230,
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
 
-    angle: {
-        0: 90
-    },
-    strokeDashoffset: {
-        '100%': '-100%'
-    },
-})
-let petal3 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: '#FF6800',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 528,
-    top: 230,
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
+  const CircleB = new mojs.Shape({
+    shape:        'rect',
+    left:         '70%',
+    fill:         'black',
+    radius:       50,
+    stroke:       { 'rgba(0,255,255, 1)' : 'magenta' },
+    strokeWidth:  { 15: 5 },
+    strokeDasharray: '100%',
+    strokeDashoffset: { '-100%' : '100%' },
+    angle:        { 0: 360 },
+  
+    duration:     6000,
+    repeat:       999,
+  }).play();
 
-    angle: {
-        0: 90
-    },
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
-let petal4 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'blue',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 427,
-    top: 340,
-    scale: {
-        0: 8
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
-let petal5 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'yellow',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 350,
-    top: 309,
-    scale: {
-        0: 8
-    },
-    angle: {
-        0: 45
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
-let petal6 = new mojs.Shape({
-    parent: zeParent,
-    Shape: 'circle',
-    fill: 'yellow',
-    radiusX: 2.5,
-    radiusY: 12.79,
-    left: 350,
-    top: 309,
-    scale: {
-        0: 8
-    },
-    angle: {
-        0: 270
-    },
-    duration: 1000,
-    delay: 1500,
-    easing: 'cubic.in',
-    strokeDashoffset: {
-        '-100%': '100%'
-    },
-})
+
+  const CircleC = new mojs.Shape({
+    shape:        'rect',
+    left:         '60%',
+    fill:         'black',
+    radius:       50,
+    stroke:       { 'red' : 'blue' },
+    strokeWidth:  { 15: 5 },
+    strokeDasharray: '15%',
+    strokeDashoffset: { '-100%' : '100%' },
+    angle:        { 0: 360 },
+  
+    duration:     6000,
+    repeat:       999,
+  }).play();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // La timeline
 const timeline = new mojs.Timeline({
-
-    })
-    .add(circle)
-    .add(petal1)
-    .add(petal2)
-    .add(petal3)
-    .add(petal4)
-    .add(petal5)
-    .add(petal6)
-
+    repeat: 999
+})
+    .add(CircleA) // Chaque animation est ajoutée à la timeline
+    .add(CircleB)
+    .add(CircleC)
 document.querySelector("#animProjection").addEventListener("click", function () {
 
     if (isPlaying != true) {
@@ -168,3 +88,4 @@ document.querySelector("#animProjection").addEventListener("click", function () 
     }
 
 });
+
